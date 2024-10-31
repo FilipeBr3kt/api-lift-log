f41783d477dcf27495c4a32ebb1f66bf59df8085
Filipe <filipe@evosix.com.br>
2024-10-31 15:13:22 -0300

    fix: repository response

--- src/api-log-lift.Domain/Interfaces/ITrainingExerciseRepository.cs
+++ src/api-log-lift.Domain/Interfaces/ITrainingExerciseRepository.cs
@@ -5,6 +5,6 @@ namespace api_log_lift.Domain.Interfaces;
 
 public interface ITrainingExerciseRepository
 {
-  Task<IEnumerable<TrainingExerciseResponse?>> GetByTrainingId(int trainingId, CancellationToken cancellationToken);
+  Task<IEnumerable<TrainingExercise?>> GetByTrainingId(int trainingId, CancellationToken cancellationToken);
   Task<bool> Save(TrainingExercise trainingExercise, CancellationToken cancellationToken);
 }
