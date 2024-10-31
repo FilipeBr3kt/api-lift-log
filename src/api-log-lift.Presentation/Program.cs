23e3a9b112394fee54e470c3e92caaaa5ba94982
Filipe <filipe@evosix.com.br>
2024-10-31 10:00:06 -0300

    feat: configure controllers, mediator and problem details

---
+++ src/api-log-lift.Presentation/Program.cs
@@ -0,0 +1,42 @@
+using System.Text.Json;
+using api_log_lift.Application.Queries;
+using api_log_lift.Infrastructure;
+using api_log_lift.Presentation;
+using api_log_lift.Presentation.Middleware;
+using MediatR;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services
+        .AddPresentation()
+        .AddInfrastructure(builder.Configuration)
+        .AddControllers();
+
+    builder.Services
+    .AddSwaggerGen()
+    .AddEndpointsApiExplorer()
+    .AddProblemDetails()
+    .AddExceptionHandler<ExceptionToProblemDetailsHandler>();
+
+    builder.Services.AddMediatR(typeof(GetMuscleQueryHandler).Assembly);
+}
+{
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.MapControllers();
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.UseExceptionHandler();
+    app.UseHttpsRedirection();
+    app.Run();
+}
+
+
+
+
